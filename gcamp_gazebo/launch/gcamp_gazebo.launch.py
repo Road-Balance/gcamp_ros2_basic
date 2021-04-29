aab0497d524afbee3dc7a0e406573668fb8de55b
kimsooyoung <tge1375@naver.com>
2021-04-29 09:30:57 +0900

    Update : robot wheel visual deleted / launch updated

--- gcamp_gazebo/launch/gcamp_gazebo.launch.py
+++ gcamp_gazebo/launch/gcamp_gazebo.launch.py
@@ -4,29 +4,38 @@ import os
 from pathlib import Path
 
 import launch
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
 
 
 os.chdir(os.path.join(str(Path.home()), "gcamp_ros2_ws", "src", "gcamp_ros2_basic", "gcamp_gazebo"))
 
 def generate_launch_description():
+
+    robot_file = 'skidbot.urdf'
+    package_name = 'gcamp_gazebo'
+    world_file_name = 'bocbot_office.world'
+
+    world = os.path.join(get_package_share_directory(package_name), 
+                        'worlds', world_file_name)
+    urdf = os.path.join(get_package_share_directory(package_name), 
+                        'urdf', robot_file)
+
     # TODO(anyone): Forward command line arguments once that's supported, see
     # https://github.com/ros2/launch/issues/107
     gzserver = launch.actions.ExecuteProcess(
-        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',
-         'worlds/test_world_2_sdf.world'],
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', world],
         output='screen'
     )
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                arguments=['-entity', 'gcamp_gazebo', '-file', 'urdf/tinybot_diff.xacro'],
+                arguments=['-entity', 'gcamp_gazebo', '-file', urdf],
                 output='screen')
 
     return LaunchDescription([
         gzserver,
-        # spawn_entity
+        spawn_entity
     ])
