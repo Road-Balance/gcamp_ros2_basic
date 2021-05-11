199111996573947379d76173063b79459e5d41dd
kimsooyoung <tge1375@naver.com>
2021-05-11 20:09:39 +0900

    Ongoing : ICP added from PyICP

--- py_topic_pkg/py_topic_pkg/laser_icp.py
+++ py_topic_pkg/py_topic_pkg/laser_icp.py
@@ -6,7 +6,7 @@ import math
 import rclpy
 import numpy as np
 from rclpy.node import Node
-
+from py_topic_pkg.ICP import icp
 from sensor_msgs.msg import LaserScan
 
 class LaserSubscriber(Node):
@@ -32,57 +32,11 @@ class LaserSubscriber(Node):
         if self.prev_laser is None:
             self.prev_laser = laser_pad
         else:
-            R = self.best_fit_transform(self.prev_laser, laser_pad)
-            self.psi += math.atan2(R[0,0],R[1,0])
-            print(self.psi)
-    
-    # # https://github.com/ClayFlannigan/icp/blob/master/icp.py
-    def best_fit_transform(self, A, B):
-        '''
-        Calculates the least-squares best-fit transform that maps corresponding points A to B in m spatial dimensions
-        Input:
-        A: Nxm numpy array of corresponding points
-        B: Nxm numpy array of corresponding points
-        Returns:
-        T: (m+1)x(m+1) homogeneous transformation matrix that maps A on to B
-        R: mxm rotation matrix
-        t: mx1 translation vector
-        '''
-
-        assert A.shape == B.shape
-
-        # get number of dimensions
-
-        # translate points to their centroids
-        centroid_A = np.mean(A, axis=0)
-        centroid_B = np.mean(B, axis=0)
-        AA = A - centroid_A
-        BB = B - centroid_B
-
-        # rotation matrix
-        H = np.dot(AA.T, BB)
-        # print(f'H : {H}')
-
-        m = H.shape[1]
-
-        U, S, Vt = np.linalg.svd(H)
-        R = np.dot(Vt.T, U.T)
-        # print(Vt)
-
-        # special reflection case
-        if np.linalg.det(R) < 0:
-            Vt[m-1,:] *= -1
-            R = np.dot(Vt.T, U.T)
-
-        # # translation
-        # t = centroid_B.T - np.dot(R,centroid_A.T)
-
-        # # homogeneous transformation
-        # T = np.identity(m+1)
-        # T[:m, :m] = R
-        # T[:m, m] = t
-
-        return R
+            # pass
+            loop_transform, _, _ = icp(self.prev_laser, laser_pad, max_iterations=20)
+            # R = self.best_fit_transform(self.prev_laser, laser_pad)
+            # self.psi += math.atan2(R[0,0],R[1,0])
+            print(loop_transform)
 
 def main(args=None):
     rclpy.init(args=args)
