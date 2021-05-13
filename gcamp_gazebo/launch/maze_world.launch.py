1ee96abeba445c1d49625b7cc7923fd234318072
kimsooyoung <tge1375@naver.com>
2021-05-13 17:00:56 +0900

    Diffbot really done

--- gcamp_gazebo/launch/maze_world.launch.py
+++ gcamp_gazebo/launch/maze_world.launch.py
@@ -4,11 +4,14 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
 
 # this is the function launch  system will look for
 
-
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     rviz_file = "diffbot.rviz"
     robot_file = "diffbot.urdf"
@@ -24,10 +27,11 @@ def generate_launch_description():
 
     # read urdf contents because to spawn an entity in
     # gazebo we need to provide entire urdf as string on  command line
-    xml = open(urdf, "r").read()
+    robot_desc = open(urdf, "r").read()
+
 
     # double quotes need to be with escape sequence
-    xml = xml.replace('"', '\\"')
+    xml = robot_desc.replace('"', '\\"')
 
     # 90 degree rotation for initial robot pose
     orientation = "{ orientation : { x : 0.0, y: 0.0, z: 0.707, w: 0.707 } }"
@@ -35,9 +39,18 @@ def generate_launch_description():
     # this is argument format for spwan_entity service
     spwan_args = '{name: "diffbot", xml: "' + xml + '", initial_pose :' + orientation + '}'
 
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+        arguments=[urdf],
+    )
+
     # create and return launch description object
     return LaunchDescription(
         [
+            # robot state publisher allows robot model spawn in RVIZ
+            robot_state_publisher_node,
             # start gazebo, notice we are using libgazebo_ros_factory.so instead of libgazebo_ros_init.so
             # That is because only libgazebo_ros_factory.so contains the service call to /spawn_entity
             ExecuteProcess(
