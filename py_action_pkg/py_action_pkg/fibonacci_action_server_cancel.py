c21400f69e2860f0d61d0e209cccc9eb614070e0
kimsooyoung <tge1375@naver.com>
2021-05-27 23:45:47 +0900

    Update : own Copyright

--- py_action_pkg/py_action_pkg/fibonacci_action_server_cancel.py
+++ py_action_pkg/py_action_pkg/fibonacci_action_server_cancel.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env/ python3
 
+# Copyright 2021 Kimsooyoung
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Also Referenced ROS Documents 
 # https://docs.ros.org/en/foxy/Tutorials/Actions/Writing-a-Py-Action-Server-Client.html#id4
 
 import time
 
 import rclpy
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 
@@ -19,6 +35,7 @@ class FibonacciActionServer(Node):
             self,
             Fibonacci,
             'fibonacci',
+            callback_group=ReentrantCallbackGroup(),
             execute_callback=self.execute_callback,
             goal_callback=self.goal_callback,
             cancel_callback=self.cancel_callback)
