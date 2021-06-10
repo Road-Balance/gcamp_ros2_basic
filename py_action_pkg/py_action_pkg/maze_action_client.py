f0dfcd6fa80710bd28a07a24c95af1c930901ccf
kimsooyoung <tge1375@naver.com>
2021-06-10 19:44:38 +0900

    Fix : lint fixed

--- py_action_pkg/py_action_pkg/maze_action_client.py
+++ py_action_pkg/py_action_pkg/maze_action_client.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env/ python3
 
+from custom_interfaces.action import Maze
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
-
-from custom_interfaces.action import Maze
 
 """
 Maze.action structure
@@ -18,17 +17,18 @@ string feedback_msg
 
 
 class MazeActionClient(Node):
+
     def __init__(self):
-        super().__init__("maze_action_client")
-        self._action_client = ActionClient(self, Maze, "maze_action")
-        self.get_logger().info("=== Maze Action Client Started ====")
+        super().__init__('maze_action_client')
+        self._action_client = ActionClient(self, Maze, 'maze_action')
+        self.get_logger().info('=== Maze Action Client Started ====')
 
     def send_goal(self, turning_list):
         goal_msg = Maze.Goal()
         goal_msg.turning_sequence = turning_list
 
         if self.action_client.wait_for_server(10) is False:
-            self.get_logger().error("Server Not exists")
+            self.get_logger().error('Server Not exists')
 
         self._send_goal_future = self._action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback
@@ -38,23 +38,23 @@ class MazeActionClient(Node):
 
     def feedback_callback(self, feedback_message):
         feedback = feedback_message.feedback
-        print(f"Received feedback: {feedback.feedback_msg}")
+        self.get_logger().info(f'Received feedback: {feedback.feedback_msg}')
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
 
         if not goal_handle.accepted:
-            self.get_logger().info("Goal rejected")
+            self.get_logger().info('Goal rejected')
             return
 
-        self.get_logger().info("Goal accepted")
+        self.get_logger().info('Goal accepted')
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         result = future.result().result
-        self.get_logger().warn(f"Action Done !! Result: {result.success}")
+        self.get_logger().warn(f'Action Done !! Result: {result.success}')
         rclpy.shutdown()
 
 
@@ -65,19 +65,22 @@ def main(args=None):
     user_inputs = []
     # Input Logic
     try:
-        print("Enter numbers [or stop] : ")
+        maze_action_client.get_logger().info('Enter numbers [or stop] : ')
 
         while True:
             user_inputs.append(int(input()))
     # if the input is not-integer, just print the list
-    except:
-        print("Your sequence list : ", user_inputs)
+    except Exception:
+        maze_action_client.get_logger().info('Your sequence list : ', user_inputs)
 
-    print(f"==== Sending Goal ====")
-    future = maze_action_client.send_goal(user_inputs)
+    maze_action_client.get_logger().info('==== Sending Goal ====')
+    maze_action_client.send_goal(user_inputs)
+
+    # You can get Future for additional functoins
+    # future = maze_action_client.send_goal(user_inputs)
 
     rclpy.spin(maze_action_client)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- py_action_pkg/py_action_pkg/maze_action_server.py
+++ py_action_pkg/py_action_pkg/maze_action_server.py
@@ -32,7 +32,7 @@ Maze.action structure
 
 class MazeActionServer(Node):
     def __init__(self):
-        super().__init__("maze_action_server")
+        super().__init__('maze_action_server')
 
         self.yaw = 0.0
         self.forward_distance = 0.0
@@ -41,22 +41,22 @@ class MazeActionServer(Node):
         self.loop_rate = self.create_rate(5, self.get_clock())
 
         self.laser_sub = self.create_subscription(
-            LaserScan, "/diffbot/scan", self.laser_sub_cb, 10
+            LaserScan, '/diffbot/scan', self.laser_sub_cb, 10
         )
 
         self.odom_sub = self.create_subscription(
-            Odometry, "/diffbot/odom", self.odom_sub_cb, 10
+            Odometry, '/diffbot/odom', self.odom_sub_cb, 10
         )
 
-        self.cmd_vel_pub = self.create_publisher(Twist, "/diffbot/cmd_vel", 10)
+        self.cmd_vel_pub = self.create_publisher(Twist, '/diffbot/cmd_vel', 10)
 
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.publish_callback)
 
         self._action_server = ActionServer(
-            self, Maze, "maze_action", self.execute_callback
+            self, Maze, 'maze_action', self.execute_callback
         )
-        self.get_logger().info("=== Maze Action Server Started ====")
+        self.get_logger().info('=== Maze Action Server Started ====')
 
     def laser_sub_cb(self, data):
         self.forward_distance = data.ranges[360]
@@ -65,13 +65,13 @@ class MazeActionServer(Node):
     def odom_sub_cb(self, data):
         orientation = data.pose.pose.orientation
         _, _, self.yaw = euler_from_quaternion(orientation)
-        # print(f"yaw : {self.yaw}")
+        # print(f'yaw : {self.yaw}')
 
     def publish_callback(self):
         self.cmd_vel_pub.publish(self.twist_msg)
 
     def turn_robot(self, euler_angle):
-        print(f"Robot Turns to {euler_angle}")
+        print(f'Robot Turns to {euler_angle}')
 
         turn_offset = 100
 
@@ -101,15 +101,15 @@ class MazeActionServer(Node):
         time.sleep(1)
 
     def execute_callback(self, goal_handle):
-        self.get_logger().info("Executing goal...")
+        self.get_logger().info('Executing goal...')
 
         feedback = Maze.Feedback()
-        feedback.feedback_msg = ""
+        feedback.feedback_msg = ''
 
         for _, val in enumerate(goal_handle.request.turning_sequence):
-            print(f"Current Cmd: {val}")
+            print(f'Current Cmd: {val}')
 
-            feedback.feedback_msg = f"Turning {direction_str_dict[val]}"
+            feedback.feedback_msg = f'Turning {direction_str_dict[val]}'
 
             self.turn_robot(direction_dict[val])
             self.parking_robot()
@@ -122,12 +122,12 @@ class MazeActionServer(Node):
 
         if sum(center_pixel) < 300 and center_pixel[1] > 100:
             goal_handle.succeed()
-            self.get_logger().warn("==== Succeed ====")
+            self.get_logger().warn('==== Succeed ====')
             result = Maze.Result()
             result.success = True
         else:
             goal_handle.abort()
-            self.get_logger().error("==== Fail ====")
+            self.get_logger().error('==== Fail ====')
             result = Maze.Result()
             result.success = False
 
@@ -146,7 +146,7 @@ def main(args=None):
         try:
             executor.spin()
         except KeyboardInterrupt:
-            maze_action_server.get_logger().info("Keyboard Interrupt (SIGINT)")
+            maze_action_server.get_logger().info('Keyboard Interrupt (SIGINT)')
         finally:
             executor.shutdown()
             maze_action_server._action_server.destroy()
@@ -155,5 +155,5 @@ def main(args=None):
         rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
