064b153046860b1cb92d925386d3b533a33c811b
kimsooyoung <tge1375@naver.com>
2021-06-28 23:48:55 +0900

    Lint check passs

--- cpp_first_pkg/src/lifecycle.cpp
+++ cpp_first_pkg/src/lifecycle.cpp
@@ -1,3 +1,18 @@
+// Copyright 2021 Seoul Business Agency Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
 
 class Talker : public rclcpp::Node
@@ -17,7 +32,8 @@ public:
   {
     RCLCPP_WARN(this->get_logger(), "Node Constructor");
 
-    m_timer = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&Talker::timer_callback, this));
+    m_timer = this->create_wall_timer(std::chrono::milliseconds(500),
+      std::bind(&Talker::timer_callback, this));
   }
 
   ~Talker(){
--- cpp_first_pkg/src/simple_oop.cpp
+++ cpp_first_pkg/src/simple_oop.cpp
@@ -1,3 +1,18 @@
+// Copyright 2021 Seoul Business Agency Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
 
 class Talker : public rclcpp::Node
@@ -15,7 +30,8 @@ private:
 public:
   Talker() : Node("simple_oop_node")
   {
-    m_timer = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&Talker::timer_callback, this));
+    m_timer = this->create_wall_timer(std::chrono::milliseconds(500),
+      std::bind(&Talker::timer_callback, this));
   }
 };
 
