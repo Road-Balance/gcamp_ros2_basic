d07011566820938d7c7fd98cabf3edea0fa44fbb
kimsooyoung <tge1375@naver.com>
2021-07-03 13:16:22 +0900

    String Quote Fixed

--- py_topic_pkg/py_topic_pkg/endless_cmd_vel_pub.py
+++ py_topic_pkg/py_topic_pkg/endless_cmd_vel_pub.py
@@ -15,7 +15,7 @@ class CmdVelPublisher(Node):
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.publish_callback)
         self.get_logger().info(
-            " DriveForward Node Started, move forward endless \n"
+            'DriveForward Node Started, move forward endless \n'
         )
 
     def publish_callback(self):
