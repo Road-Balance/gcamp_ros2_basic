ea7fa9f989e8ba58e80c05b03fb8790a41f6a995
kimsooyoung <tge1375@naver.com>
2021-07-17 20:04:24 +0900

    launch added

--- py_action_pkg/setup.py
+++ py_action_pkg/setup.py
@@ -1,3 +1,7 @@
+from glob import glob
+
+import os
+
 from setuptools import setup
 
 package_name = 'py_action_pkg'
@@ -10,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
