d202323f654f5a66db29601db43b8dc047037e02
kimsooyoung <tge1375@naver.com>
2022-07-13 02:37:37 +0900

    Comment Added

--- basic_action_pkg/basic_action_pkg/turtle_turning_client.py
+++ basic_action_pkg/basic_action_pkg/turtle_turning_client.py
@@ -36,20 +36,25 @@ class TurtleRotate(Node):
 
     def __init__(self):
         super().__init__('turtle_rotate_client')
+        
+        # Create Action Client
         self.action_client = ActionClient(self, RotateAbsolute, 'turtle1/rotate_absolute')
         self.get_logger().info('=== Turtle Rotate Action Client Started ====')
 
     def send_goal(self, theta):
         goal_msg = RotateAbsolute.Goal()
         goal_msg.theta = theta
-
+        
+        # Wait for server first
         if self.action_client.wait_for_server(10) is False:
             self.get_logger().error('Server Not exists')
 
+        # Send Goal then receive future
         self._send_goal_future = self.action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback
         )
 
+        # Done callback Add
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def feedback_callback(self, feedback_msg):
@@ -57,6 +62,7 @@ class TurtleRotate(Node):
         self.get_logger().info(f'Received feedback: {feedback.remaining}')
 
     def goal_response_callback(self, future):
+
         goal_handle = future.result()
 
         if not goal_handle.accepted:
@@ -65,6 +71,7 @@ class TurtleRotate(Node):
 
         self.get_logger().info('Goal accepted')
 
+        # Add Result cb
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
